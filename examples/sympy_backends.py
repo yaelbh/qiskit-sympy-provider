3145a59a8568b8b66a764ed199848a09b1c21f97
Jay Gambetta <jay.gambetta@us.ibm.com>
2018-10-11 22:11:48 -0400

    Adding changes

--- examples/sympy_backends.py
+++ examples/sympy_backends.py
@@ -20,3 +20,22 @@ SyQ = SympyProvider()
 print(SyQ.backends())
 
 print(SyQ.backends(name='statevector_simulator_sympy'))
+
+backend = SyQ.get_backend('statevector_simulator_sympy')
+print(backend)
+
+
+# gets the name of the backend.
+print(backend.name())
+
+# gets the status of the backend.
+print(backend.status())
+
+# returns the provider of the backend
+print(backend.provider) 
+
+# gets the configuration of the backend.
+print(backend.configuration())
+
+# gets the properties of the backend.
+print(backend.properties())
